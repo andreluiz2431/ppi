105b5a36be7d216740dbdbfb98503a0d2e4aad4f
André Luiz <42250150+endputz2431@users.noreply.github.com>
2018-11-16 19:52:03 -0200

    Add files via upload

--- cadastrar.php
+++ cadastrar.php
@@ -9,54 +9,73 @@ include "./conexao.php";
         <meta charset="UTF-8">
         <title>Cadastro</title>
         <style>
-            body{
-                background-color: lightblue;
-            }
-            .botao{
-                background-color: lightblue;
+            @import url(https://fonts.googleapis.com/css?family=Open+Sans);
+
+            * { -webkit-box-sizing:border-box; -moz-box-sizing:border-box; -ms-box-sizing:border-box; -o-box-sizing:border-box; box-sizing:border-box; }
+
+            body { 
+                width: 100%;
+                
+                font-family: 'Open Sans', sans-serif;
+                background: #092756;
+                background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%),-moz-linear-gradient(top,  rgba(57,173,219,.25) 0%, rgba(42,60,87,.4) 100%), -moz-linear-gradient(-45deg,  #670d10 0%, #092756 100%);
+                background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -webkit-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -webkit-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+                background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -o-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -o-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+                background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -ms-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -ms-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+                background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), linear-gradient(to bottom,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), linear-gradient(135deg,  #670d10 0%,#092756 100%);
+                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3E1D6D', endColorstr='#092756',GradientType=1 );
             }
 
-            input[type=text], input[type=password], input[type=date]{
-                width: 20%;
-                padding: 15px 100px 8px 8px;
+            input, textarea { 
+                margin-left: 10;
+                margin-right: 10;
+
+                margin-bottom: 10px; 
+                background: rgba(0,0,0,0.3);
                 border: none;
-                background: #f1f1f1;
-            }
-            input[type=text]:focus, input[type=password]:focus, input[type=date]:focus{
-                background-color: #ddd;
                 outline: none;
+                padding: 10px;
+                font-size: 13px;
+                color: #fff;
+                text-shadow: 1px 1px 1px rgba(0,0,0,0.3);
+                border: 1px solid rgba(0,0,0,0.3);
+                border-radius: 4px;
+                box-shadow: inset 0 -5px 45px rgba(100,100,100,0.2), 0 1px 1px rgba(255,255,255,0.2);
+                -webkit-transition: box-shadow .5s ease;
+                -moz-transition: box-shadow .5s ease;
+                -o-transition: box-shadow .5s ease;
+                -ms-transition: box-shadow .5s ease;
+                transition: box-shadow .5s ease;
             }
-            .baaaaaaa{
-                background-color: gray;
-                color: #FFF;
-
-                border-radius: 30px;
+            input:focus { box-shadow: inset 0 -5px 45px rgba(100,100,100,0.4), 0 1px 1px rgba(255,255,255,0.2); }
+            legend{
+                font-size: 30;
+                color: whitesmoke;
             }
             label{
                 color: darkgray;
-                font-size: 20;
             }
-
         </style>
     </head>
     <body>
         <form method="GET" action="cadastrar.php">
+            
             <br><br>
-            <div style="margin-right: 200; margin-left: 200;" align="center">
-                <fieldset>
+            <div align="center">
+                
                     <legend><strong>Cadastre-se</strong></legend>
                     <br>
-                    <label>Data de cadastro:</label><br> <!-- colocar no banco e definir o 'name' -->
+                    <label>Data de cadastro:</label> <!-- colocar no banco e definir o 'name' -->
                     <input type="date" name="nome">
                     <br><br>
-                    <label>Nome:</label><br>
-                    <input type="text" name="nome">
+                    
+                    <input type="text" name="nome" placeholder="Nome">
                     <br><br>
-                    <label>RG:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="RG">
                     <br><br>
-                    <label>CPF:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome"placeholder="CPF">
                     <br><br>
                     <label>Sexo:</label> <!-- colocar no banco e definir o 'name' -->
                     <br>
@@ -66,11 +85,7 @@ include "./conexao.php";
                     <input type="radio" name="sexo" value="f">
                     <label>Outro</label>
                     <input type="radio" name="sexo" value="o">
-                    <?php
-                    if ($_GET['sexo'] == 'o') {      // arrumar!!!!!!!!!!!
-                        echo "<input type='text' name='outroSexo'>";
-                    }
-                    ?>
+                    
                     <br><br>
                     <label>Estado civil:</label><br> <!-- colocar no banco e definir o 'name' -->
                     <select>
@@ -86,57 +101,57 @@ include "./conexao.php";
                         <option name='xxx'>xxx</option>
                     </select>
                     <br><br>
-                    <label>Email:</label><br>
-                    <input type="email" name="email">
+                    
+                    <input type="email" name="email" placeholder="Email">
                     <br><br>
-                    <label>Profissão:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                     <input type="text" name="nome" placeholder="Profissão">
                     <br><br>
-                    <label>Data de nascimento:</label><br>
+                    <label>Data de nascimento:</label>
                     <input type="date" name="data">
                     <br><br>
-                    <label>Endereço:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="Endereço">
                     <br><br>
-                    <label>Número de residência:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="Número de residência">
                     <br><br>
-                    <label>Bairro:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="Bairro">
                     <br><br>
-                    <label>CEP:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="CEP">
                     <br><br>
-                    <label>Cidade:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="Cidade">
                     <br><br>
-                    <label>UF:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="UF">
                     <br><br>
-                    <label>Celular:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="Celular">
                     <br><br>
-                    <label>Altura:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                     <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="Altura">
                     <br><br>
-                    <label>Peso:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                    <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="Peso">
                     <br><br>
-                    <label>Dia(s) da semana de aula:</label><br> <!-- colocar no banco e definir o 'name' -->
+                    <label>Dia(s) da semana de aula:</label> <!-- colocar no banco e definir o 'name' -->
                     <input type="date" name="nome">
                     <br><br>
-                    <label>Horário de início:</label><br> <!-- colocar no banco e definir o 'name' -->
+                    <label>Horário de início:</label> <!-- colocar no banco e definir o 'name' -->
                     <input type="time" name="nome">
                     <br><br>
-                    <label>Tempo de aula:</label><br>
+                    <label>Tempo de aula:</label>
                     <input type="time" name="hora">
                     <br><br>
-                    <label>Mensalidade:</label><br> <!-- colocar no banco e definir o 'name' -->
-                    <input type="text" name="nome">
+                    <!-- colocar no banco e definir o 'name' -->
+                    <input type="text" name="nome" placeholder="Mensalidade">
                     <br><br>
                     <label>Problemas de saúde:</label><br> <!-- colocar no banco e definir o 'name' -->
                     <textarea>
-                Insira seus dados
+                
                     </textarea>
                     <br><br>
                     <label>Receber Publicidade:</label> <!-- retirar do banco -->
@@ -149,7 +164,7 @@ include "./conexao.php";
                     <br><br>
                     <input class="baaaaaaa" type="submit" value="Enviar">
                     <br>
-                </fieldset>
+                
             </div>
         </form>
         <?php
